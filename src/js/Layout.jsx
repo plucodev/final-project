fd94b6e29b185a31bd60bd30bda56c469080c079
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-01-19 14:40:16 +0000

    Updated layout.jsx

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -15,6 +15,13 @@ export class Layout extends React.Component {
 						<Switch>
 							<Route exact path="/" component={Home} />
 							<Route exact path="/home" component={Home} />
+							<Route exact path="/logout" component={Logout} />
+							<Route exact path="/login" component={Login} />
+							<Route exact path="/contact-us" component={ContactUs} />
+							<Route exact path="/checkout" component={Checkout} />
+							<Route exact path="/cart" component={Cart} />
+							<Route exact path="/products" component={Products} />
+							<Route exact path="/product/:theid" component={Product} />
 							<Route
 								exact
 								path="/how-it-works"
