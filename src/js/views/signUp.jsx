abd31fcc94c8ce8162ec299edd27f5b4f4ddcaef
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-01-24 21:23:15 +0000

    Updated signUp.jsx

--- src/js/views/signUp.jsx
+++ src/js/views/signUp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Context } from "../store/appContext.jsx";
 import { withRouter } from "react-router-dom";
+import Featured from "../component/featured.jsx";
 
 class SignUp extends React.Component {
 	render() {
@@ -146,13 +147,97 @@ class SignUp extends React.Component {
 									aria-labelledby="headingTwo"
 									data-parent="#accordionExample">
 									<div className="card-body">
-										Anim pariatur
+										<main className="login-form">
+											<div className="cotainer">
+												<div className="row justify-content-center">
+													<div className="col-md-12">
+														<div className="card">
+															<div className="card-header">
+																Register
+															</div>
+															<div className="card-body">
+																<form
+																	action=""
+																	method="">
+																	<div className="form-group row">
+																		<label
+																			htmlFor="email_address"
+																			className="col-md-4 col-form-label text-md-right">
+																			E-Mail
+																			Address
+																		</label>
+																		<div className="col-md-6">
+																			<input
+																				type="text"
+																				id="email_address"
+																				className="form-control"
+																				name="email-address"
+																				required
+																				autoFocus
+																			/>
+																		</div>
+																	</div>
+
+																	<div className="form-group row">
+																		<label
+																			htmlFor="password"
+																			className="col-md-4 col-form-label text-md-right">
+																			Password
+																		</label>
+																		<div className="col-md-6">
+																			<input
+																				type="password"
+																				id="password"
+																				className="form-control"
+																				name="password"
+																				required
+																			/>
+																		</div>
+																	</div>
+
+																	<div className="form-group row">
+																		<div className="col-md-6 offset-md-4">
+																			<div className="checkbox">
+																				<label>
+																					<input
+																						type="checkbox"
+																						name="remember"
+																					/>{" "}
+																					Remember
+																					Me
+																				</label>
+																			</div>
+																		</div>
+																	</div>
+
+																	<div className="col-md-6 offset-md-4">
+																		<button
+																			type="submit"
+																			className="btn btn-primary">
+																			Register
+																		</button>
+																		<a
+																			href="#"
+																			className="btn btn-link">
+																			Forgot
+																			Your
+																			Password?
+																		</a>
+																	</div>
+																</form>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</main>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+				<Featured />
 			</div>
 		);
 	}
