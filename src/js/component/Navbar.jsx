0a55d88c7fef41ba2b2612c89d5da8e26ac2fcd0
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-01-29 15:51:54 +0000

    Updated Log In Menu Dropdown

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -43,14 +43,75 @@ const Navbar = () => {
 								Procedures
 							</Link>
 						</li>
-						<li className="nav-item">
-							<Link to="./sign-up" className="nav-link" href="#">
-								Sign Up
+
+						<li className="nav-item dropdown">
+							<Link
+								className="nav-link dropdown-toggle"
+								to="#"
+								id="navbarDropdown"
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false">
+								Log In / Sign In
 							</Link>
+							<div className="dropdown-menu">
+								<form className="px-4 py-3">
+									<div className="form-group">
+										<label htmlFor="exampleDropdownFormEmail1">
+											Email address
+										</label>
+										<input
+											type="email"
+											className="form-control"
+											id="exampleDropdownFormEmail1"
+											placeholder="email@example.com"
+										/>
+									</div>
+									<div className="form-group">
+										<label htmlFor="exampleDropdownFormPassword1">
+											Password
+										</label>
+										<input
+											type="password"
+											className="form-control"
+											id="exampleDropdownFormPassword1"
+											placeholder="Password"
+										/>
+									</div>
+									<div className="form-group">
+										<div className="form-check">
+											<input
+												type="checkbox"
+												className="form-check-input"
+												id="dropdownCheck"
+											/>
+											<label
+												className="form-check-label"
+												htmlFor="dropdownCheck">
+												Remember me
+											</label>
+										</div>
+									</div>
+									<button
+										type="submit"
+										className="btn btn-info">
+										Log In
+									</button>
+								</form>
+								<div className="dropdown-divider" />
+								<Link className="dropdown-item" to="/sign-up">
+									New around here? Sign up
+								</Link>
+								<Link className="dropdown-item" to="#">
+									Forgot password?
+								</Link>
+							</div>
 						</li>
+
 						<li className="nav-item active">
 							<Link to="/cart" className="nav-link">
-								<i className="fas fa-shopping-cart" />
+								<i className="fab fa-opencart" />
 							</Link>
 							<span className="sr-only" />
 						</li>
@@ -62,3 +123,57 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// <li className="nav-item dropdown">
+// 							<Link
+// 								to="#"
+// 								className="nav-link dropdown-toggle"
+// 								id="navbarDropdown">
+// 								Log In / Sign Up
+// 							</Link>
+// 							<div className="dropdown-menu">
+// 								<form className="px-4 py-3">
+// 									<div className="form-group">
+// 										<label htmlFor="exampleDropdownFormEmail1">
+// 											Email address
+// 										</label>
+// 										<input
+// 											type="email"
+// 											className="form-control"
+// 											id="exampleDropdownFormEmail1"
+// 											placeholder="email@example.com"
+// 										/>
+// 									</div>
+// 									<div className="form-group">
+// 										<label htmlFor="exampleDropdownFormPassword1">
+// 											Password
+// 										</label>
+// 										<input
+// 											type="password"
+// 											className="form-control"
+// 											id="exampleDropdownFormPassword1"
+// 											placeholder="Password"
+// 										/>
+// 									</div>
+// 									<div className="form-group">
+// 										<div className="form-check">
+// 											<input
+// 												type="checkbox"
+// 												className="form-check-input"
+// 												id="dropdownCheck"
+// 											/>
+// 											<label
+// 												className="form-check-label"
+// 												htmlFor="dropdownCheck">
+// 												Remember me
+// 											</label>
+// 										</div>
+// 									</div>
+// 									<button
+// 										type="submit"
+// 										className="btn btn-primary">
+// 										Sign in
+// 									</button>
+// 								</form>
+// 							</div>
+// 						</li>
